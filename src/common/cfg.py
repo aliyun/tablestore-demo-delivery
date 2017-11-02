0a2708bc6a4fe5d64738621364b891d24337a19d
Taoda <tyf00@aliyun.com>
2017-11-02 17:04:10 +0800

    workable configs
    should not be merged into master

--- src/common/cfg.py
+++ src/common/cfg.py
@@ -1,7 +1,7 @@
-OTS_ENDPOINT = 'http://$YourInstance.cn-hangzhou.ots.aliyuncs.com'
-OTS_INSTANCE = '$YourInstance'
+OTS_ENDPOINT = 'http://x-delivery-demo.cn-shanghai.ots-internal.aliyuncs.com'
+OTS_INSTANCE = 'x-delivery-demo'
 OTS_FLYWIRE_TABLE = 'Flywire'
 OTS_BUBBLE_TABLE = 'Bubble'
 
-FC_ENDPOINT = 'https://$YourUID.fc.cn-hangzhou.aliyuncs.com'
-FC_SERVICE = '$YourService'
+FC_ENDPOINT = 'https://1549066622369459.fc.cn-shanghai.aliyuncs.com'
+FC_SERVICE = 'delivery-demo'
